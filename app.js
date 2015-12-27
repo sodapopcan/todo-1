a3e2f76e98c8c45821d970c5656d3432fee8c0ff
Andrew Haust <andrewwhhaust@gmail.com>
2015-12-27 02:46:32 -0500

    Add node-postgres with example insertion

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 require('dotenv').load();
 
-console.log(process.env.DB_USER);
+var ENV = process.env;
+
+var pg = require('pg');
+var dburi = 'postgres://' + ENV.DB_USER + '@' + ENV.DB_HOST + '/' + ENV.DB_DATABASE;
+
+pg.connect(dburi, function(err, client, done) {
+  if (err) return console.error('database error', err);
+
+  client.query("INSERT INTO todos (name) VALUES('Andrew')", function(err, result) {
+    done();
+
+    if (err) return console.error("Couldn't insert", err);
+
+    console.log(result);
+  });
+});
