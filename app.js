7b183c4db83e0679861a5b7558228032f568093e
Andrew Haust <andrewwhhaust@gmail.com>
2015-12-27 04:09:34 -0500

    Add show route

--- app.js
+++ app.js
@@ -9,14 +9,22 @@ var express = require('express');
 var db = pgp('postgres://' + ENV.DB_USER + '@' + ENV.DB_HOST + '/' + ENV.DB_DATABASE);
 var app = express();
 
-app.get('/api/todos', function(req, res) {
-  db.query("SELECT * FROM todos")
+function apiCall(res, sql, params) {
+  db.query(sql, params)
     .then(function(data) {
       res.send(data);
     })
     .catch(function(err) {
       res.send("There was an error: " + err);
     });
+}
+
+app.get('/api/todos', function(req, res) {
+  apiCall(res, "SELECT * FROM todos");
+});
+
+app.get('/api/todos/:id', function(req, res) {
+  apiCall(res, "SELECT * FROM todos WHERE id = $1", [req.params.id]);
 });
 
 app.listen(3000);
