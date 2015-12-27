6dc9c0c628ef20db809d312b7e0be7206b537344
Andrew Haust <andrewwhhaust@gmail.com>
2015-12-27 02:53:50 -0500

    Add lodash with an SQL SELECT example

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ require('dotenv').load();
 var ENV = process.env;
 
 var pg = require('pg');
+var _ = require('lodash');
+
 var dburi = 'postgres://' + ENV.DB_USER + '@' + ENV.DB_HOST + '/' + ENV.DB_DATABASE;
 
 pg.connect(dburi, function(err, client, done) {
   if (err) return console.error('database error', err);
 
-  client.query("INSERT INTO todos (name) VALUES('Andrew')", function(err, result) {
+  client.query("SELECT * FROM todos", function(err, result) {
     done();
 
-    if (err) return console.error("Couldn't insert", err);
-
-    console.log(result);
+    _.map(result.rows, function(todo) {
+      console.log(todo.id, todo.name);
+    });
   });
 });
