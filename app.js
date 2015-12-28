657b7a8a623c29e258eeea06b8aec02fd89267bc
Andrew Haust <andrewwhhaust@gmail.com>
2015-12-28 00:06:17 -0500

    Refactor: Couple CRUD to View

--- app.js
+++ app.js
@@ -23,20 +23,44 @@ const Todo = sequelize.define('todo', {
   name: Sequelize.STRING
 });
 
+const TodoItem = sequelize.define('todo_item', {
+  body: Sequelize.STRING,
+  complete: Sequelize.BOOLEAN
+});
+
+Todo.hasMany(TodoItem, { as: 'Items' });
+TodoItem.belongsTo(Todo);
+
 sequelize.sync();
 
 
 /*  Routes  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
+function cruddy_create(uri, model, token) {
+  app.post(uri, (req, res) => {
+    model.create(req.body[token]).then(data => res.send(data)).catch(e => res.send(e));
+  });
+}
+
+function cruddy_update(uri, model, token) {
+  app.put(uri, (req, res) => {
+    model.findOne({ id: req.params.id }).then(instance => {
+      instance.update(req.body[token]).then(data => res.send(data));
+    });
+  });
+}
+
+/* ToDo */
 // List all
 app.get('/api/todos', (req, res) => {
   Todo.findAll().then(todos => res.send(todos));
 });
-
 // List one
 app.get('/api/todos/:id', (req, res) => {
   Todo.findOne({ id: req.params.id  }).then(todo => res.send(todo));
 });
+cruddy_create('/api/todos', Todo, 'todo');
+cruddy_update('/api/todos/:id', Todo, 'todo');
 
 // Create
 app.post('/api/todos', (req, res) => {
